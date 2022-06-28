3e64354160e17b9983f18fb7a995de7746b68555
Benedikt Wirsing <benedikt.wirsing@gmail.com>
2022-06-29 00:36:14 +0200

    Größenerkennung anhand A4 Papier

---
+++ synthetic_data/utlis.py
@@ -0,0 +1,10 @@
+import cv2
+from cv2 import GaussianBlur
+from cv2 import Canny
+import numpy as np
+
+def getContours(img,cThr=[100,100], showCanny=False): #100,100 Verändern für Edge Detection bei Canny #ShowCanny=True für Vorschau
+    imgGray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) #Bild wird schwarz weiß
+    imgBlur=cv2.GaussianBlur(imgGray,(5,5),1) #Kernel 5*5
+    imgCanny=cv2.Canny(imgBlur,cThr[0], cThr[1])
+    if showCanny: cv2.imshow('Canny', imgCanny)
