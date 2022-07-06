a242711f8a99a8b75f58e3aa40738280f41ede00
nicolasdoenerundpizzahaus <nicolas.wild@tech11.com>
2022-07-07 00:46:02 +0200

    code prettier

--- frontend/prototype/lib/projectView/gallery.dart
+++ frontend/prototype/lib/projectView/gallery.dart
@@ -9,21 +9,19 @@ class Gallery extends StatefulWidget {
   int length;
   Gallery(this.src, [this.length = 8000]);
 
+  @override
   _GalleryState createState() {
-    print(src);
-    // TODO: implement createState
-    return _GalleryState(src, length);
+    return _GalleryState();
   }
 }
 
 class _GalleryState extends State<Gallery> {
-  String src;
-  int length;
-  _GalleryState(this.src, [this.length = 8000]);
   var galleryList = [];
 
   /// befüllt die Liste "galleryList" mit den Bildern aus dem angegbenen Ordner
   List<dynamic> getList() {
+    String src = widget.src;
+    int length = widget.length;
     try {
       FileUtils.getImages(src).then((loadedImages) {
         setState(() {
