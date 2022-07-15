a36e62574eaf790e4c12e8e4799ee1b0079d2210
Benedikt Wirsing <benedikt.wirsing@gmail.com>
2022-07-15 15:57:08 +0200

    Erklärungen im Code (Neuladen weil nicht geht)

--- synthetic_data/utlis_Angepasst.py
+++ synthetic_data/utlis_Angepasst.py
@@ -6,20 +6,22 @@ import numpy as np
 #Notiz Nicolas ist COOOL
 
 def getContours(img,cThr=[100,100], showCanny=False, minArea=1000, filter=0, draw=False): #100,100 Verändern für Edge Detection bei Canny #ShowCanny=True für Vorschau #Filter für geometrische Figuren
+    #Bildbearbeitung
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Bild wird schwarz weiß
     imgBlur=cv2.GaussianBlur(imgGray,(5,5),1) #Kernel 5*5
-    imgCanny=cv2.Canny(imgBlur,cThr[0], cThr[1])
+    imgCanny=cv2.Canny(imgBlur,cThr[0], cThr[1]) #Cannyfilter mit Werten aus Header
     #Smoothing der Kanten
-    kernel= np.ones((5,5))
-    imgDial = cv2.dilate(imgCanny,kernel,iterations=3)
-    imgThre=cv2.erode(imgDial,kernel,iterations=2)
+    kernel= np.ones((5,5)) #Neues Array 5*5 groß
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=3) #Neues Bild, welches verwässert ist
+    imgThre=cv2.erode(imgDial,kernel,iterations=2) #Neues Bild, welches verschlankt ist
     if showCanny:
          #cv2.imshow('Canny', imgCanny)
          cv2.imwrite("F:\\Projektseminar\\Edge Detection\\Kanten.jpg", imgCanny) 
 
-    contours,hierarchy= cv2.findContours(imgThre,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #Filter für bestimmte Formen
+    contours,hierarchy= cv2.findContours(imgThre,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #Extrahiert die Koordinaten der Eckpunkte in ein Array [] final Contours; Filter für bestimmte Formen
+    print("contours ist",contours)
     finalContours= []
-    for i in contours:
+    for i in contours: #i ist jede einzelne Kante [(i),(i),(i)]
         area=cv2.contourArea(i)
         if area > minArea:
             peri=cv2.arcLength(i,True) #True weil Area geschlossen ist
