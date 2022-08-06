62e96ce4470d7af9d9cc4b366c8c8e3bf811ded8
nicolasdoenerundpizzahaus <nicolas.wild@tech11.com>
2022-08-06 10:55:15 +0200

    zwischenstand

--- frontend/prototype/lib/screen_load_project/_main_view.dart
+++ frontend/prototype/lib/screen_load_project/_main_view.dart
@@ -1,9 +1,13 @@
+import 'dart:io';
+
+import 'package:meta/meta.dart';
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:prototype/backend/data_base_functions.dart';
 import 'package:prototype/backend/helper_objects.dart';
 import 'package:prototype/components/gallery.dart';
 import 'package:prototype/screen_load_project/projectMap.dart';
+import 'package:sqflite/sqflite.dart';
 import 'package:tflite/tflite.dart';
 
 import '../components/gallery2.dart';
@@ -22,6 +26,9 @@ class _ProjectViewState extends State<ProjectView> {
   double totalSquareMeters = 0.0;
   double totalPrice = 0.0;
   var images = [];
+  String outcome = "konnte nicht ermittelt werden";
+  File pickedImage = File("");
+  bool imagesLoaded = false;
 
   @override
   initState() {
@@ -48,14 +55,31 @@ class _ProjectViewState extends State<ProjectView> {
           imagelist.forEach((element) {
             setState(() {
               images.add(element);
+              imagesLoaded = true;
             });
           })
         });
+
     return images;
   }
 
   loadAiModel() async {
     var model = await Tflite.loadModel(model: "assets/material_model.tflite");
+    print("++++++++++++++++++++++++++++++++" + model.toString());
+  }
+
+// applyModelOnImage methode im inder video wird nirgends aufgraufen
+  applyModelOnImage() async {
+    File sth = File("assets/bathRoom1.jpg");
+    // woher weiß das programm im inder video, welches modell gemeint ist, er stellt keine verknüpfung modell, das modell wird nirgends gespeichert, auch
+    // sonst wird die load model mthode nirgends nochmal verwendet außer in init state
+    var res = await Tflite.runModelOnImage(path: sth.toString());
+
+    print("-------------------------------------" + res.toString());
+
+    setState(() {
+      outcome = res.toString();
+    });
   }
 
   double getSquareMeter() {
@@ -89,8 +113,9 @@ class _ProjectViewState extends State<ProjectView> {
 
   @override
   Widget build(BuildContext context) {
+    // applyModelOnImage();
     Map<String, dynamic> content = widget.content;
-    print("------------------------------------$images");
+
     totalPrice = getPrice();
     // getJsonValues();
     return Scaffold(
@@ -102,9 +127,11 @@ class _ProjectViewState extends State<ProjectView> {
       body: Column(children: [
         // test to check if Project view is able to load data, which had been entered before
         Center(child: ProjectMap()),
+        // Image(image: AssetImage('assets/bathRoom1.jpg')),
         Text("Auftraggeber: " + content["client"]),
         Text("Quadratmeter: " + totalSquareMeters.toString()),
         Text("Preis: " + totalPrice.toString()),
+        Text("KI-Ergebnis: " + outcome),
         Container(
           margin: const EdgeInsets.all(10.0),
           //    child: Text("Adresse: " + element + "straße"),
