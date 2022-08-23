6c66cef14a34704ac043b59018e8349cbeea7bee
Benedikt Wirsing <benedikt.wirsing@gmail.com>
2022-08-23 14:18:20 +0200

    komplette Objekterkennung + Img. Aug

--- synthetic_data/utlis_Angepasst.py
+++ synthetic_data/utlis_Angepasst.py
@@ -3,43 +3,47 @@ from cv2 import GaussianBlur
 from cv2 import Canny
 import numpy as np
 
-def getContours(img,cThr=[100,100], showCanny=False, minArea=1000, filter=0, draw=False): #100,100 Verändern für Edge Detection bei Canny #ShowCanny=True für Vorschau #Filter für geometrische Figuren
+#Notiz Nicolas ist COOOL
+
+def getContours(img,cThr=[100,100], showCanny=True, minArea=1000, filter=0, draw=False): #100,100 Verändern für Edge Detection bei Canny #ShowCanny=True für Vorschau #Filter für geometrische Figuren
+    #Bildbearbeitung
     imgGray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY) #Bild wird schwarz weiß
     imgBlur=cv2.GaussianBlur(imgGray,(5,5),1) #Kernel 5*5
-    imgCanny=cv2.Canny(imgBlur,cThr[0], cThr[1])
+    imgCanny=cv2.Canny(imgBlur,cThr[0], cThr[1]) #Cannyfilter mit Werten aus Header
     #Smoothing der Kanten
-    kernel= np.ones((5,5))
-    imgDial = cv2.dilate(imgCanny,kernel,iterations=3)
-    imgThre=cv2.erode(imgDial,kernel,iterations=2)
+    kernel= np.ones((5,5)) #Neues Array 5*5 groß
+    imgDial = cv2.dilate(imgCanny,kernel,iterations=3) #Neues Bild, welches verwässert ist
+    imgThre=cv2.erode(imgDial,kernel,iterations=2) #Neues Bild, welches verschlankt ist 
     if showCanny:
          #cv2.imshow('Canny', imgCanny)
-         cv2.imwrite("F:\\Projektseminar\\Edge Detection\\Kanten.jpg", imgCanny) 
+         cv2.imwrite("F:\\Projektseminar\\Edge Detection\\Kanten2.jpg", imgCanny) 
 
-    contours,hierarchy= cv2.findContours(imgThre,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #Filter für bestimmte Formen
+    contours,hierarchy= cv2.findContours(imgThre,cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE) #Extrahiert die Koordinaten der Eckpunkte in ein Array [] final Contours; Filter für bestimmte Formen
+    #print("contours ist",contours)
     finalContours= []
-    for i in contours:
-        area=cv2.contourArea(i)
+    for i in contours: #i ist jede einzelne Kante [(i),(i),(i)]
+        area=cv2.contourArea(i) #Area ist Float
         if area > minArea:
-            peri=cv2.arcLength(i,True) #True weil Area geschlossen ist
-            approx =cv2.approxPolyDP(i,0.02*peri,True) #0.02 ist die Resolution
-            bbox=cv2.boundingRect(approx)
+            peri=cv2.arcLength(i,True) #True weil Area geschlossen ist; gibt Länge zurück [Float]
+            approx =cv2.approxPolyDP(i,0.02*peri,True) #0.02 ist die Resolution; approximiert die Form
+            bbox=cv2.boundingRect(approx) #Überlagert rechteckige Bounding Box
             if filter >0:
                 if len(approx) == filter:
                     finalContours.append([len(approx), area, approx, bbox, i])
             else:   
-                finalContours.append([len(approx), area, approx, bbox, i])
+                finalContours.append([len(approx), area, approx, bbox, i]) #Dimensionen sind Länge, Fläche, Approx (array), BBOX (Array), i (Array)
     finalContours = sorted(finalContours,key= lambda x:x[1], reverse=True) #Reverse=True für Absteigend
     if draw:
         for con in finalContours:
-            cv2.drawContours(img,con[4],-1,(0,0,255),3) #Farbe (Rot); Dicke (3)
+            cv2.drawContours(img,con[4],-1,(0,0,255),3) #Farbe (Rot); Dicke (3); er malt i weil 4. Eintrag in Final Contours
 
-    return img, finalContours
+    return img, finalContours, imgCanny
 
 def reorder(myPoints):
     #print(myPoints.shape)
-    myPointsNew=np.zeros_like(myPoints)
-    myPoints=myPoints.reshape((4,2))
-    add=myPoints.sum(1)
+    myPointsNew=np.zeros_like(myPoints) #nimmt myPoints und überschreibt es mit Nullen
+    myPoints=myPoints.reshape((4,2)) #Konvertierung in 4x2 Matrix
+    add=myPoints.sum(1) 
     myPointsNew[0]=myPoints[np.argmin(add)]
     myPointsNew[3]=myPoints[np.argmax(add)]
     diff = np.diff(myPoints,axis=1)
@@ -49,6 +53,7 @@ def reorder(myPoints):
 
 def warpImg (img,points,w,h): #Höhe und Breite des Bilds 
     # print(points)
+    #print(points)
     points=reorder(points)
     pts1=np.float32(points)
     pts2= np.float32([[0,0],[w,0],[0,h],[w,h]])
